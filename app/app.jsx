c176ba3310252123e559798567cfa8b5a753ab97
BoyceLyu <lvzhendong.bl@gmail.com>
2015-06-01 23:26:55 +0800

    继续code

--- app/app.jsx
+++ app/app.jsx
@@ -26,5 +26,174 @@
                 }, this);
             }
         }
-    })
+
+        var TodoApp = React.createClass({
+        mixins: [BackboneMixin],
+        getBackboneCollections: function () {
+            return [this.props.todos];
+        },
+
+        getInitialState: function () {
+            return {editing: null};
+        },
+
+        componentDidMount: function () {
+            var Router = Backbone.Router.extend({
+                routes: {
+                    '': 'all',
+                    'active': 'active',
+                    'completed': 'completed'
+                },
+                all: this.setState.bind(this, {nowShowing: app.ALL_TODOS}),
+                active: this.setState.bind(this, {nowShowing: app.ACTIVE_TODOS}),
+                completed: this.setState.bind(this, {nowShowing: app.COMPLETED_TODOS})
+            });
+
+            new Router();
+            Backbone.history.start();
+
+            this.props.todos.fetch();
+        },
+
+        componentDidUpdate: function () {
+            // If saving were expensive we'd listen for mutation events on Backbone and
+            // do this manually. however, since saving isn't expensive this is an
+            // elegant way to keep it reactively up-to-date.
+            this.props.todos.forEach(function (todo) {
+                todo.save();
+            });
+        },
+
+        handleNewTodoKeyDown: function (event) {
+            if (event.which !== ENTER_KEY) {
+                return;
+            }
+
+            var val = this.refs.newField.getDOMNode().value.trim();
+            if (val) {
+                this.props.todos.create({
+                    title: val,
+                    completed: false,
+                    order: this.props.todos.nextOrder()
+                });
+                this.refs.newField.getDOMNode().value = '';
+            }
+
+            return false;
+        },
+
+        toggleAll: function (event) {
+            var checked = event.target.checked;
+            this.props.todos.forEach(function (todo) {
+                todo.set('completed', checked);
+            });
+        },
+
+        edit: function (todo, callback) {
+            // refer to todoItem.jsx `handleEdit` for the reason behind the callback
+            this.setState({editing: todo.get('id')}, callback);
+        },
+
+        save: function (todo, text) {
+            todo.save({title: text});
+            this.setState({editing: null});
+        },
+
+        cancel: function () {
+            this.setState({editing: null});
+        },
+
+        clearCompleted: function () {
+            this.props.todos.completed().forEach(function (todo) {
+                todo.destroy();
+            });
+        },
+
+        render: function () {
+            var footer;
+            var main;
+            var todos = this.props.todos;
+
+            var shownTodos = todos.filter(function (todo) {
+                switch (this.state.nowShowing) {
+                case app.ACTIVE_TODOS:
+                    return !todo.get('completed');
+                case app.COMPLETED_TODOS:
+                    return todo.get('completed');
+                default:
+                    return true;
+                }
+            }, this);
+
+            var todoItems = shownTodos.map(function (todo) {
+                return (
+                    <TodoItem
+                        key={todo.get('id')}
+                        todo={todo}
+                        onToggle={todo.toggle.bind(todo)}
+                        onDestroy={todo.destroy.bind(todo)}
+                        onEdit={this.edit.bind(this, todo)}
+                        editing={this.state.editing === todo.get('id')}
+                        onSave={this.save.bind(this, todo)}
+                        onCancel={this.cancel}
+                    />
+                );
+            }, this);
+
+            var activeTodoCount = todos.reduce(function (accum, todo) {
+                return todo.get('completed') ? accum : accum + 1;
+            }, 0);
+
+            var completedCount = todos.length - activeTodoCount;
+
+            if (activeTodoCount || completedCount) {
+                footer =
+                    <TodoFooter
+                        count={activeTodoCount}
+                        completedCount={completedCount}
+                        nowShowing={this.state.nowShowing}
+                        onClearCompleted={this.clearCompleted}
+                    />;
+            }
+
+            if (todos.length) {
+                main = (
+                    <section id="main">
+                        <input
+                            id="toggle-all"
+                            type="checkbox"
+                            onChange={this.toggleAll}
+                            checked={activeTodoCount === 0}
+                        />
+                        <ul id="todo-list">
+                            {todoItems}
+                        </ul>
+                    </section>
+                );
+            }
+
+            return (
+                <div>
+                    <header id="header">
+                        <h1>todos</h1>
+                        <input
+                            ref="newField"
+                            id="new-todo"
+                            placeholder="What needs to be done?"
+                            onKeyDown={this.handleNewTodoKeyDown}
+                            autoFocus={true}
+                        />
+                    </header>
+                    {main}
+                    {footer}
+                </div>
+            );
+        }
+
+        React.renderComponent(
+            <TodoApp todos={app.todos} />,
+            document.getElementById('todoapp')
+        );
+    );
+    })();
 
--- app/todoItem.jsx
+++ app/todoItem.jsx
@@ -29,13 +29,48 @@ var todoItem = React.createClass({
 		this.setState({editText:this.props.todo.get("title")});
 	},
 
-	
+	handleKeyDown: function (event) {
+			if (event.which === ESCAPE_KEY) {
+				this.setState({editText: this.props.todo.get('title')});
+				this.props.onCancel();
+			} else if (event.which === ENTER_KEY) {
+				this.handleSubmit();
+			}
+		},
 
-	render: function() {
-		return (
-			<div />
-		);
-	}
+		handleChange: function (event) {
+			this.setState({editText: event.target.value});
+		},
+
+	render: function () {
+			return (
+				<li className={React.addons.classSet({
+					completed: this.props.todo.get('completed'),
+					editing: this.props.editing
+				})}>
+					<div className="view">
+						<input
+							className="toggle"
+							type="checkbox"
+							checked={this.props.todo.get('completed')}
+							onChange={this.props.onToggle}
+						/>
+						<label onDoubleClick={this.handleEdit}>
+							{this.props.todo.get('title')}
+						</label>
+						<button className="destroy" onClick={this.props.onDestroy} />
+					</div>
+					<input
+						ref="editField"
+						className="edit"
+						value={this.state.editText}
+						onBlur={this.handleSubmit}
+						onChange={this.handleChange}
+						onKeyDown={this.handleKeyDown}
+					/>
+				</li>
+			);
+		}
 
 });
 
